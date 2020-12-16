364c75c19bbebd5aa4b1f227dbe3343f9fc5ee79
alexKozachuk <grayid@icloud.com>
2020-12-16 07:54:46 +0200

    first commit

---
+++ Homework2/Model/User.swift
@@ -0,0 +1,32 @@
+//
+//  User.swift
+//  Homework2
+//
+//  Created by Sasha on 14/12/2020.
+//
+
+import Foundation
+
+enum UserType {
+    case regular
+    case admin
+}
+
+class User {
+    var name: String
+    var password: String
+    var bets: [String] = []
+    var isBanned: Bool = false
+    var userType: UserType
+    
+    init(name: String, password: String, userType: UserType) {
+        self.name = name
+        self.password = password
+        self.userType = userType
+    }
+    
+    func makeBet(_ item: String) {
+        bets.append(item)
+    }
+    
+}
---
+++ Homework2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework2
+//
+//  Created by Sasha on 14/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
